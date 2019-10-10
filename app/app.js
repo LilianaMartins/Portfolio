561e02c15e0828f98e39793f4f1d256b1ca013eb
Liliana Martins <lilianasrmartins@gmail.com>
2019-10-10 04:26:07 +0100

    added porfolio div
    added div and a couple of projects - still working on format

--- app/app.js
+++ app/app.js
@@ -80,3 +80,33 @@ window.onload = function() {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+
+
+
+
+//fadeIn for projects
+
+
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('.fadeIn').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},2000);
+
+            }
+
+        });
+
+    });
+
+});
